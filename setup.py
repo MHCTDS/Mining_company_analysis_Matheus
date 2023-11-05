05f0cca9062bd331bd243292397a5ac641cccfdb
“MHCTDS” <1MHCTDS8@gmail.com>
2023-11-05 19:08:23 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An analysis of a gold mining company with the aim to predict the purity of final extraction based on initial factors using ML.Data was provided by Zyfra',
+    author='Matheus Henrique Cajueiro Tobias de Souza',
+    license='',
+)
